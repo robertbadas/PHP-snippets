ba85b1b3606c0eb0429fc7290d255551a89b5e61
robertbadas <robertbadas@gmail.com>
2017-03-09 23:30:22 +0100

    Create zip.php

---
+++ zip.php
@@ -0,0 +1,35 @@
+<?php
+$zip_file           = dirname(__FILE__) . '/FILENAME';
+
+// Get real path for our folder
+$rootPath = realpath($zip_file);
+
+// Initialize archive object
+$zip = new ZipArchive();
+$zip->open('file.zip', ZipArchive::CREATE | ZipArchive::OVERWRITE);
+
+// Create recursive directory iterator
+/** @var SplFileInfo[] $files */
+$files = new RecursiveIteratorIterator(
+    new RecursiveDirectoryIterator($rootPath),
+    RecursiveIteratorIterator::LEAVES_ONLY
+);
+
+foreach ($files as $name => $file)
+{
+    // Skip directories (they would be added automatically)
+    if (!$file->isDir())
+    {
+        // Get real and relative path for current file
+        $filePath = $file->getRealPath();
+        $relativePath = substr($filePath, strlen($rootPath) + 1);
+
+        // Add current file to archive
+        $zip->addFile($filePath, $relativePath);
+    }
+}
+
+// Zip archive will be created only after closing object
+$zip->close();
+echo 'ZIP complete!';
+?>
